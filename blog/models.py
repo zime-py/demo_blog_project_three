7cd91e68f5fd5abf8c55e06921b2633f9dfbdf60
mahmud555 <mahmudhossain838@gmail.com>
2020-01-25 11:25:57 +0600

    hello

--- blog/models.py
+++ blog/models.py
@@ -42,6 +42,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
